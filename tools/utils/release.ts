5c4664ef4d6377107e22452b3349e88e30f22104
Val Neekman <val@neekware.com>
2021-03-28 15:41:17 -0400

    Initial release script

---
+++ tools/utils/release.ts
@@ -0,0 +1,101 @@
+import { writeFile, writeFileSync, readFileSync } from 'fs';
+import { pick } from 'lodash';
+import * as path from 'path';
+import * as program from 'commander';
+import { parse, SemVer } from 'semver';
+import { distDir, execute, projPkgJson } from './utils';
+
+const moduleBuildPath = path.join(distDir, 'libs', program.lib);
+const modulePkgPath = require(path.join(moduleBuildPath, 'package.json'));
+const publishOptions = `--access public --non-interactive --no-git-tag-version `;
+
+async function syncPackageData() {
+  let modulePkg = require(modulePkgPath);
+
+  // update common attributes
+  const parentInfo = pick(projPkgJson, [
+    'author',
+    'version',
+    'license',
+    'homepage',
+    'repository',
+    'contributors',
+    'keywords',
+    'bugs',
+  ]);
+
+  modulePkg = { ...modulePkg, ...parentInfo };
+  // flush new files to build dir of each package
+  await writeFile(modulePkgPath, JSON.stringify(modulePkg, null, 2), () => {
+    console.error(`Flushed package.json  ...`);
+  });
+
+  await writeFileSync(
+    path.join(moduleBuildPath, './README.md'),
+    readFileSync('./README.md')
+  );
+}
+
+async function buildPackage() {
+  if (program.build) {
+    const cmd = `ng build --prod`;
+    console.log(cmd);
+    await execute(cmd).catch((error) => {
+      console.log(`Failed to build ${program.lib} ... ${error}`);
+      return false;
+    });
+  }
+  return true;
+}
+
+async function getDevVersion() {
+  const lastCommit = await execute('git rev-parse HEAD');
+  const commitHash = lastCommit.toString().trim().slice(0, 10);
+
+  // 1.0.0 to become 1.0.0+dev.commitHash
+  const version: SemVer = parse(projPkgJson.version);
+  const semVer = `${version.major}.${version.minor}.${version.patch}`;
+  const devVersion = `${semVer}-dev-${commitHash}`;
+  return devVersion;
+}
+
+async function main() {
+  const built = await buildPackage();
+  if (!built) {
+    return 1;
+  }
+
+  await syncPackageData();
+
+  let newVersion = projPkgJson.version;
+  let publishCmd = `yarn publish ${publishOptions} --new-version ${newVersion} --tag latest`;
+  if (program.dev) {
+    newVersion = await getDevVersion();
+    publishCmd = `yarn publish ${publishOptions} --new-version ${newVersion} --tag dev`;
+  }
+
+  if (program.publish) {
+    console.log('Publishing new version', newVersion);
+    console.log(publishCmd);
+    await execute(`cd ${moduleBuildPath} && ${publishCmd}`).catch((error) => {
+      console.log(`Failed to publish package. ${error}`);
+    });
+
+    if (!program.dev) {
+      console.log('You probably want to also tag the version now:');
+      console.log(
+        ` git tag -a ${newVersion} -m 'version ${newVersion}' && git push --tags`
+      );
+    }
+  }
+}
+
+program
+  .version('0.0.1', '-v, --version')
+  .option('-n', '--lib <lib>', 'Library name to push to npmjs.org')
+  .option('-b, --build', 'Build the lib')
+  .option('-p, --publish', 'Publish @<lib-name>@latest')
+  .option('-d, --dev', 'Publish @<lib-name>@next')
+  .parse(process.argv);
+
+main();
