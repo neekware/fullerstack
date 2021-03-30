0f456be164417f0e7ecdeb4d7671a76c26365474
Val Neekman <val@neekware.com>
2021-03-30 11:26:01 -0400

    Switch back to express for now

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,9 +14,10 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || environment.port || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+  await app.listen(port);
+  console.log(
+    `Application is running on: ${await app.getUrl()}/${globalPrefix}`
+  );
 }
 
 bootstrap();
