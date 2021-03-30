e4086b2fe257e0a6337c4cc960e7057189e46cf1
Val Neekman <val@neekware.com>
2021-03-30 16:50:11 -0400

    flipping back to express to make gql work

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,24 +1,20 @@
-import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(environment.serverOptions)
-  );
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
   app.setGlobalPrefix(environment.prefix);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || environment.port || 3333;
   await app.listen(port);
+
   console.log(
-    `Application is running on: ${await app.getUrl()}/${environment.prefix}`
+    `Application is running on: http://localhost:${port}/${environment.prefix}`
   );
 }
+
 bootstrap();
