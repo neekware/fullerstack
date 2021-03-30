f05b58cbf532d2be632f464593332d1f4801b131
Val Neekman <val@neekware.com>
2021-03-30 10:31:33 -0400

    switching to Fastify instead of Express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,20 +3,47 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+// import { Logger } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
 
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const globalPrefix = 'api';
+//   app.setGlobalPrefix(globalPrefix);
+//   const port = process.env.PORT || environment.port || 3333;
+//   await app.listen(port, () => {
+//     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+//   });
+// }
+
+// bootstrap();
+
+import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(environment.serverOptions)
+  );
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || environment.port || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-}
 
+  app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT || environment.port || 3333;
+  await app.listen(port);
+  console.log(
+    `Application is running on: ${await app.getUrl()}/${globalPrefix}`
+  );
+}
 bootstrap();
