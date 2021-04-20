96f923d3af1ef8e5fdfe5942fd8171d7ffec6f8e
Val Neekman <val@neekware.com>
2021-04-20 11:26:09 -0400

    Build ci pass

--- libs/nsx-user/src/lib/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
-
-@Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-}
--- libs/nsx-user/src/lib/user.module.ts
+++ libs/nsx-user/src/lib/user.module.ts
@@ -1,11 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 
 @Global()
 @Module({
-  controllers: [UserController],
   providers: [UserService, UserResolver],
   exports: [UserService, UserResolver],
 })
