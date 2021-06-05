563a3a74166181b67fa9c57696e5fe071018c4e7
Val Neekman <val@neekware.com>
2021-06-05 15:09:33 -0400

    ngx auth test

--- libs/ngx-auth/src/lib/auth.interceptor.spec.ts
+++ libs/ngx-auth/src/lib/auth.interceptor.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptor } from './auth.interceptor';
+import { AuthService } from './auth.service';
+import { AuthEffectsService } from './store/auth-state.effect';
 
 describe('AuthInterceptor', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      providers: [AuthInterceptor],
+      providers: [
+        AuthInterceptor,
+        {
+          provide: AuthService,
+          useValue: {},
+        },
+        {
+          provide: AuthEffectsService,
+          useValue: {},
+        },
+      ],
     })
   );
 
