fd8a41f3e7c8ed9134c7c38b37d3bfafe7a33d68
Val Neekman <val@neekware.com>
2021-06-15 22:08:33 -0400

    test clean up

--- libs/ngx-gql/src/lib/gql.interceptor.ts
+++ libs/ngx-gql/src/lib/gql.interceptor.ts
@@ -6,16 +6,13 @@ import {
   HttpResponse,
 } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
-import { HttpStatusCode } from '@fullerstack/agx-dto';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { gqlErrorsConverter } from './gql.error';
-import { GqlRequestBody } from './gql.model';
 import { GqlService } from './gql.service';
 
 @Injectable({ providedIn: 'root' })
-export class GqlErrorInterceptor implements HttpInterceptor {
+export class GqlInterceptor implements HttpInterceptor {
   private gql: GqlService;
 
   constructor(private injector: Injector) {
@@ -26,22 +23,5 @@ export class GqlErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(gqlErrorsConverter());
-  }
-}
-
-@Injectable({ providedIn: 'root' })
-export class GqlSuccessInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      map((event) => {
-        if (event instanceof HttpResponse && event?.type && event.status === HttpStatusCode.OK) {
-          const operationName = (request.body as GqlRequestBody)?.operationName;
-          if (operationName) {
-            event = new HttpResponse({ ...event, body: event.body.data[operationName] });
-          }
-        }
-        return event;
-      })
-    );
   }
 }
