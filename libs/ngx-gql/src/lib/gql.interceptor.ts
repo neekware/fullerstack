ab1481566c95a171f8520b89eec07502fa0868db
Val Neekman <val@neekware.com>
2021-06-17 11:22:35 -0400

    docs

--- libs/ngx-gql/src/lib/gql.interceptor.ts
+++ libs/ngx-gql/src/lib/gql.interceptor.ts
@@ -6,7 +6,18 @@ import { gqlErrorsConverter } from './gql.error';
 
 @Injectable({ providedIn: 'root' })
 export class GqlInterceptor implements HttpInterceptor {
+  /**
+   *
+   * @param request outgoing request object
+   * @param next next interceptor to call
+   * @returns observable of http event type
+   */
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    /**
+     * Graphql Errors are wrapped in a 200 http status code.
+     * This interceptor will pull the errors out, and re-raise them as http error
+     * Other interceptors then can handle them as regular http errors
+     */
     return next.handle(request).pipe(gqlErrorsConverter());
   }
 }
