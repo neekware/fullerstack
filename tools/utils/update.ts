d3d61325f8990cdd93c988872707987e903dd825
Val Neekman <val@neekware.com>
2021-06-18 16:35:33 -0400

    update tool

---
+++ tools/utils/update.ts
@@ -0,0 +1,70 @@
+/**
+ * @license
+ * Copyright Neekware Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at http://neekware.com/license/MIT.html
+ */
+
+import * as program from 'commander';
+
+import { execute, projPkgJson } from './util';
+
+const depSkip = ['@angular', '@nrwl', 'rxjs'];
+const devDepSkip = ['@angular', '@nrwl', 'typescript'];
+
+const DEBUG = false;
+
+/**
+ * Update packages
+ */
+async function update2Latest(pkgName: string, devDep = true) {
+  const pkgMgr = program.manager || 'yarn';
+  let cmd = `yarn add ${pkgName}`;
+  let devFlag = '-D';
+  if (pkgMgr === 'npm') {
+    cmd = `npm install ${pkgName}`;
+    devFlag = '--save-dev';
+  }
+
+  if (devDep) {
+    cmd = `${cmd} ${devFlag}`;
+  }
+
+  console.log(`Updating ${pkgName} ...`);
+  await execute(cmd, !DEBUG);
+}
+
+async function updateDependencies() {
+  if (projPkgJson.hasOwnProperty('dependencies')) {
+    const updateList = Object.keys(projPkgJson.dependencies).filter(
+      (key) => !depSkip.some((name) => key.startsWith(name))
+    );
+
+    await update2Latest(updateList.join(' '), false);
+  }
+}
+
+async function updateDevDependencies() {
+  if (projPkgJson.hasOwnProperty('devDependencies')) {
+    const updateList = Object.keys(projPkgJson.dependencies).filter(
+      (key) => !devDepSkip.some((name) => key.startsWith(name))
+    );
+    await update2Latest(updateList.join(' '), false);
+  }
+}
+
+async function main() {
+  await updateDependencies();
+  await updateDevDependencies();
+}
+
+program
+  .version('0.0.1', '-v, --version')
+  .option('-m', '--manager <manager>', 'Package manager, [yarn | npm] (default: yarn)')
+  .parse(process.argv);
+
+main().catch((err) => {
+  console.error(`Error updating package.json`, err);
+  process.exit(111);
+});
