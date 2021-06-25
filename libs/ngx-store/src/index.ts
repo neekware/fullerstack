d03338910f1f47d127e4dbe2ff3167d1eab542e3
Val Neekman <val@neekware.com>
2021-06-24 21:38:04 -0400

    prepare package

--- libs/ngx-store/src/index.ts
+++ libs/ngx-store/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/ngx-store.module';
+export * from './lib/store.default';
+export * from './lib/store.freeze';
+export * from './lib/store.model';
+export * from './lib/store.module';
+export * from './lib/store.service';
+export * from './lib/store.util';
