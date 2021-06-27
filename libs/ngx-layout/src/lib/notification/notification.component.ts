804c11d263f77e1794b68262cf2ada0638c712f7
Val Neekman <val@neekware.com>
2021-06-27 14:04:39 -0400

    alert

--- libs/ngx-layout/src/lib/notification/notification.component.ts
+++ libs/ngx-layout/src/lib/notification/notification.component.ts
@@ -6,7 +6,11 @@
  * that can be found at http://neekware.com/license/PRI.html
  */
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { sample } from 'lodash-es';
+import { Observable, Subject, timer } from 'rxjs';
+import { map, share, takeUntil } from 'rxjs/operators';
 
 import { LayoutService } from '../layout.service';
 
@@ -16,6 +20,113 @@ import { LayoutService } from '../layout.service';
   styleUrls: ['./notification.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotificationComponent {
-  constructor(readonly layout: LayoutService) {}
+export class NotificationComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<boolean>();
+  activeTabIndex = 1;
+  currentTime$: Observable<Date>;
+
+  notifications = [
+    {
+      title: 'Account',
+      message: 'Account needs post registration verification',
+      icon: 'account',
+      color: 'primary',
+    },
+    {
+      title: 'Settings',
+      message: 'Settings updated',
+      icon: 'cogs',
+      color: 'accent',
+    },
+    {
+      title: 'Profile',
+      message: 'Profile needs updated',
+      icon: 'information-outline',
+      color: 'warn',
+    },
+    {
+      title: 'Order accepted',
+      message: 'Order to buy 100 APPL @ $207',
+      icon: 'currency-usd',
+      color: 'primary',
+    },
+    {
+      title: 'Order executed',
+      message: 'Purchased 100 APPL shares',
+      icon: 'check-outline',
+      color: 'primary',
+    },
+  ];
+  markets = [
+    {
+      ticker: 'NYSE',
+      price: '12,134.00',
+      change: '+1.63%',
+      color: 'green',
+    },
+    {
+      ticker: 'DOW JONES',
+      price: '12,134.00',
+      change: '-1.63%',
+      color: 'red',
+    },
+    {
+      ticker: 'NASDAQ',
+      price: '12,134.00',
+      change: '0.0%',
+      color: 'gray',
+    },
+  ];
+  stocks = [
+    {
+      ticker: 'TSLA',
+      price: '359.44',
+      change: '+1.63%',
+      color: 'green',
+    },
+    {
+      ticker: 'AMZN',
+      price: '1,865.90',
+      change: '0.0%',
+      color: 'gray',
+    },
+    {
+      ticker: 'AAPL',
+      price: '207.31',
+      change: '-1.63%',
+      color: 'red',
+    },
+    {
+      ticker: 'GOOGL',
+      price: '1,265.37',
+      change: '5.63%',
+      color: 'green',
+    },
+  ];
+
+  hasNewNotification = true;
+
+  constructor(readonly layout: LayoutService) {
+    if (this.hasNewNotification) {
+      this.activeTabIndex = 1;
+    }
+  }
+
+  ngOnInit() {
+    this.currentTime$ = timer(0, 1000).pipe(
+      map(() => new Date()),
+      share(),
+      takeUntil(this.destroy$)
+    );
+  }
+
+  handleTapChange(event: MatTabChangeEvent) {
+    this.activeTabIndex = event.index;
+    this.hasNewNotification = sample([true, false]);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
