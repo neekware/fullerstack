0298a2bc264b634551e9821e3c7b502343de369e
Val Neekman <val@neekware.com>
2021-06-30 21:17:36 -0400

    clean up

--- libs/ngx-i18n/src/lib/i18n.util.ts
+++ libs/ngx-i18n/src/lib/i18n.util.ts
@@ -1,4 +1,12 @@
 /**
+ * @license
+ * Copyright Neekware Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by a proprietary notice
+ * that can be found at http://neekware.com/license/PRI.html
+ */
+
+/**
  * Wrapper for translation extractor tools such as @biesbjerg/ngx-translate-extract
  * @param key - string to be translated
  */
