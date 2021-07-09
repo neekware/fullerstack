9b0678684e2cb45821ee9604cb9e2eadef3c9390
Val Neekman <val@neekware.com>
2021-07-09 15:59:06 -0400

    clean up

--- libs/nxs-i18n/src/lib/i18n.default.ts
+++ libs/nxs-i18n/src/lib/i18n.default.ts
@@ -10,7 +10,7 @@ import { I18nConfig } from './i18n.model';
 
 export const DefaultI18nConfig: I18nConfig = {
   defaultLanguage: 'en',
-  availableLanguages: ['de', 'en', 'es', 'fa', 'fr', 'he', 'zh-hans'],
-  enabledLanguages: ['de', 'en', 'es', 'fa', 'fr', 'he', 'zh-hans'],
+  availableLanguages: ['en'],
+  enabledLanguages: ['en'],
   translationDirectory: 'assets/i18n/',
 };
