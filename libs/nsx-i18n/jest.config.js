fcc488d3647ccb925e2a662b8dd52f238c5f9117
Val Neekman <val@neekware.com>
2021-09-30 13:18:10 -0400

    extract nx configs to standalone files

--- libs/nsx-i18n/jest.config.js
+++ libs/nsx-i18n/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nxs-i18n',
+  displayName: 'nsx-i18n',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/nxs-i18n',
+  coverageDirectory: '../../coverage/libs/nsx-i18n',
 };
--- libs/nxs-i18n/README.md
+++
@@ -1,7 +0,0 @@
-# nxs-i18n
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test nxs-i18n` to execute the unit tests via [Jest](https://jestjs.io).
