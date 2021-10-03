d71ad2ce79037b3faf964559127ad94c7c47a2d9
Val Neekman <val@neekware.com>
2021-10-03 08:33:57 -0400

    clean up

--- apps/avidtrader/src/app/pages/annotate/annotate.module.ts
+++ apps/avidtrader/src/app/pages/annotate/annotate.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AnnotatorModule } from '@fullerstack/ngx-annotator';
+import { MaterialModule } from '@fullerstack/ngx-material';
+import { SharedModule } from '@fullerstack/ngx-shared';
 
 import { AnnotateComponent } from './annotate.component';
 
 @NgModule({
-  imports: [CommonModule, AnnotatorModule],
+  imports: [CommonModule, MaterialModule, SharedModule, AnnotatorModule],
   declarations: [AnnotateComponent],
 })
 export class AnnotateModule {}
