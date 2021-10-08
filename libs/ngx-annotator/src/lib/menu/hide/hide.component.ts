88f7862cb4e9e3490ba19ffa3c634348ca896950
Val Neekman <val@neekware.com>
2021-10-07 20:12:40 -0400

    clean up

--- libs/ngx-annotator/src/lib/menu/hide/hide.component.ts
+++ libs/ngx-annotator/src/lib/menu/hide/hide.component.ts
@@ -7,11 +7,18 @@
  */
 
 import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { i18nExtractor as _ } from '@fullerstack/ngx-i18n';
 import { shakeAnimations } from '@fullerstack/ngx-shared';
 import { Subject } from 'rxjs';
 
 import { ButtonType } from '../../annotator.model';
 import { AnnotatorService } from '../../annotator.service';
+
+interface ActionButton {
+  type: ButtonType;
+  icon: string;
+  label: string;
+}
 
 @Component({
   selector: 'fullerstack-hide-menu',
@@ -23,6 +30,44 @@ import { AnnotatorService } from '../../annotator.service';
 export class HideMenuComponent implements OnDestroy {
   private destroy$ = new Subject<boolean>();
 
+  actionButtons: ActionButton[] = [
+    {
+      type: 'trash',
+      icon: 'trash-can-outline',
+      label: _('COMMON.TRASH'),
+    },
+    {
+      type: 'undo',
+      icon: 'undo',
+      label: _('COMMON.UNDO'),
+    },
+    {
+      type: 'redo',
+      icon: 'redo',
+      label: _('COMMON.REDO'),
+    },
+    {
+      type: 'lineWidth',
+      icon: 'format-line-weight',
+      label: _('COMMON.LINE_WEIGHT'),
+    },
+    {
+      type: 'cursor',
+      icon: 'cursor-default-click',
+      label: _('COMMON.CURSOR'),
+    },
+    {
+      type: 'fullscreen',
+      icon: 'arrow-expand-all',
+      label: _('COMMON.FULLSCREEN'),
+    },
+    {
+      type: 'refresh',
+      icon: 'web-refresh',
+      label: _('COMMON.REFRESH'),
+    },
+  ];
+
   constructor(readonly annotation: AnnotatorService) {}
 
   toggleButtonVisibility(event: Event, buttonType: ButtonType) {
@@ -33,6 +78,10 @@ export class HideMenuComponent implements OnDestroy {
         [buttonType]: !this.annotation.state.buttonVisibility[buttonType],
       },
     });
+  }
+
+  isButtonVisible(buttonType: ButtonType) {
+    return this.annotation.state.buttonVisibility[buttonType];
   }
 
   ngOnDestroy() {
--- libs/ngx-annotator/src/lib/menu/position/position.component.ts
+++ libs/ngx-annotator/src/lib/menu/position/position.component.ts
@@ -7,11 +7,19 @@
  */
 
 import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { i18nExtractor as _ } from '@fullerstack/ngx-i18n';
 import { shakeAnimations } from '@fullerstack/ngx-shared';
 import { Subject } from 'rxjs';
 
 import { MenuPosition } from '../../annotator.model';
 import { AnnotatorService } from '../../annotator.service';
+
+interface SupportedCorner {
+  position: MenuPosition;
+  icon: string;
+  class: string;
+  label: string;
+}
 
 @Component({
   selector: 'fullerstack-menu-position',
@@ -23,6 +31,33 @@ import { AnnotatorService } from '../../annotator.service';
 export class MenuPositionComponent implements OnDestroy {
   private destroy$ = new Subject<boolean>();
 
+  corners: SupportedCorner[] = [
+    {
+      position: 'top-right',
+      icon: 'select-drag',
+      class: 'flip-vertical',
+      label: _('COMMON.POSITION.TOP_RIGHT'),
+    },
+    {
+      position: 'top-left',
+      icon: 'select-drag',
+      class: 'flip-vertical-horizontal',
+      label: _('COMMON.POSITION.TOP_LEFT'),
+    },
+    {
+      position: 'bottom-right',
+      icon: 'select-drag',
+      class: '',
+      label: _('COMMON.POSITION.BOTTOM_RIGHT'),
+    },
+    {
+      position: 'bottom-left',
+      icon: 'select-drag',
+      class: 'flip-vertical',
+      label: _('COMMON.POSITION.BOTTOM_LEFT'),
+    },
+  ];
+
   constructor(readonly annotation: AnnotatorService) {}
 
   setPosition(event: Event, position: MenuPosition) {
@@ -31,6 +66,10 @@ export class MenuPositionComponent implements OnDestroy {
       ...this.annotation.state,
       position,
     });
+  }
+
+  isPosition(position: MenuPosition) {
+    return this.annotation.state.position === position;
   }
 
   toggleVertical(event: Event) {
