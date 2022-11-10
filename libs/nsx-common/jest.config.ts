c07f899d5a26524f3bba7ad783297a64c93b613d
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2022-11-10 10:37:38 -0500

    add new jest configs

---
+++ libs/nsx-common/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'common',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/nsx-common',
+};
