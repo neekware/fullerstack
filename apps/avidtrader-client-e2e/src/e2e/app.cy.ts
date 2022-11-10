bc465f3d3c3dbd2ec22574016a0b6fed466382b5
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2022-11-10 17:15:25 -0500

    update cypress e2e

--- apps/avidtrader-client-e2e/src/e2e/app.cy.ts
+++ apps/avidtrader-client-e2e/src/e2e/app.cy.ts
@@ -13,9 +13,9 @@ describe('avidtrader-client', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to avidtrader-client!');
+    getGreeting().contains('Financial Market Research Portal');
   });
 });
