a4f8233ed7455bf2df29819e3940f6c25d55e868
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2022-11-10 19:19:49 -0500

    kicking a change to test action e2e

--- apps/avidtrader-client/src/app/app.component.ts
+++ apps/avidtrader-client/src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent implements OnInit {
     readonly system: SystemService
   ) {
     if (!this.config.options.production) {
-      /* istanbul ignore next */
+      /* Istanbul ignore next */
       this.logger.info('AppComponent starting ... ');
     }
   }
